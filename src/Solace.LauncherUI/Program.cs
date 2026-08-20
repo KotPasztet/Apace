d9310e9750a6e30d9b56647ea273f7ee1d30d021
KotPasztet <gdylf.pl@gmail.com>
2026-08-20 13:03:30 +0200

    fix: add missing using for IClaimsTransformation in LauncherUI

--- src/Solace.LauncherUI/Program.cs
+++ src/Solace.LauncherUI/Program.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting.Server;
